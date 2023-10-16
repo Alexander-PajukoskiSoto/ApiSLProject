1cb8ea14cf8b32d2fbec2c87e691cb9811b505fc
Alexander Pajukoski Soto <alexander.pajukoskisoto@elev.ga.ntig.se>
2023-10-16 14:44:10 +0200

    All fetched up

--- js/page1.js
+++ js/page1.js
@@ -14,3 +14,15 @@ const timeUpdate = ()=>{
 }
 timeUpdate();
 setInterval(timeUpdate , 900);
+
+fetch("https://api.sl.se/api2/typeahead.json?key=57dcc022c3a741a69222d78e5a189749&searchstring=flemmingsberg")
+.then(res => res.json())
+.then(res => console.log(res.ResponseData));
+
+fetch("https://api.sl.se/api2/realtimedeparturesV4.json?key=70bdaec5bfac4a329b4e63101cce107d&siteid=7000&timewindow=30")
+.then(hudRes => hudRes.json())
+.then(hudRes => console.log(hudRes));
+
+fetch("https://api.sl.se/api2/realtimedeparturesV4.json?key=70bdaec5bfac4a329b4e63101cce107d&siteid=7006&timewindow=30")
+.then(flemRes => flemRes.json())
+.then(flemRes => console.log(flemRes));
