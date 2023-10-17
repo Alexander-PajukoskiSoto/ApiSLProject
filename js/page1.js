293e67b3d6ab7a850af99788e2359996099fe426
Alexander Pajukoski Soto <alexander.pajukoskisoto@elev.ga.ntig.se>
2023-10-17 10:32:50 +0200

    Confusion

--- js/page1.js
+++ js/page1.js
@@ -20,13 +20,11 @@ const hudRes = fetch("https://api.sl.se/api2/realtimedeparturesV4.json?key=70bda
 
 fetch("https://api.sl.se/api2/realtimedeparturesV4.json?key=70bdaec5bfac4a329b4e63101cce107d&siteid=7006&timewindow=30")
 .then(flemRes => flemRes.json())
-// .then(data => console.log(data))
+
 .then(data => {
     data.ResponseData.Buses.forEach(info => {
         fillDivBusFunc(info)
     });
-
-    
 });
 
 
@@ -34,7 +32,23 @@ const fillDivBusFunc = (data)=>{
     let fillDivBus = document.getElementById("slFillDivBus");
     let leftDiv= document.createElement('div');
     let rightDiv= document.createElement('div');
+    let div = document.createElement('div')
     let icon= document.createElement('i');
+
+
+    //Classes
     icon.setAttribute('class',data.GroupOfLine)
-    fillDivBus.append(icon);
+    let redBus = document.querySelector(".null");
+    let blueBus =document.querySelector(".blåbuss");
+    let eBus =document.querySelector(".Ersättningsbuss");
+
+    //append
+    div.append(leftDiv,rightDiv)
+    leftDiv.append(icon)
+    fillDivBus.append(div);
+ 
+    redBus.setAttribute('class','fa-solid fa-bus');
+    blueBus.setAttribute('class','fa-solid fa-bus');
+    eBus.setAttribute('class','fa-solid fa-bus');
+    
 }
