d658a41b20db97054eae6918248b179452aab293
Alexander Pajukoski Soto <alexander.pajukoskisoto@elev.ga.ntig.se>
2023-10-17 12:57:09 +0200

    Added like classes and stuff :trollface:

--- js/page1.js
+++ js/page1.js
@@ -33,25 +33,29 @@ const fillDivBusFunc = (data)=>{
     let fillDivBus = document.getElementById("slFillDivBus");
     let leftDiv= document.createElement('div');
     let rightDiv= document.createElement('div');
-    let div = document.createElement('div')
-    let numDiv = document.createElement('div')
-    let desDiv = document.createElement('div')
+    let div = document.createElement('div');
+    let numDiv = document.createElement('div');
+    let desDiv = document.createElement('div');
+    let dTimeDiv = document.createElement('div');
     let icon= document.createElement('i');
 
     //textNodes
     let lineNumber = document.createTextNode(data.LineNumber);
-    let destination = document.createTextNode(data.Destination)
+    let destination = document.createTextNode(data.Destination);
+    let dTime = document.createTextNode(data.DisplayTime);
 
     //Classes
-    icon.setAttribute('class', " fa-solid fa-bus")
-    numDiv.setAttribute('class',data.GroupOfLine)
+    icon.setAttribute('class', " fa-solid fa-bus");
+    numDiv.setAttribute('class',data.GroupOfLine);
 
     //append text
+    dTimeDiv.appendChild(dTime);
     desDiv.appendChild(destination);
     numDiv.appendChild(lineNumber);
 
     // append elem
     div.append(leftDiv,rightDiv);
     leftDiv.append(icon, numDiv, desDiv);
+    rightDiv.append(dTimeDiv);
     fillDivBus.append(div);
 }
